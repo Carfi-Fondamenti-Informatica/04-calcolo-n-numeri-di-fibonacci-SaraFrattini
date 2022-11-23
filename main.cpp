1a519d2302c62cd5062cdc04e71207d4722c6c0b
SaraFrattini <115411517+SaraFrattini@users.noreply.github.com>
2022-11-23 22:20:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main(){
 int n,i,a,b,c;
 cin >> n;
 a=1;
@@ -8,7 +8,7 @@ b=1;
 if (n>=2){
     cout << a << endl;
     cout << b << endl;
-    for (i=0; i=2<n; i++){
+    for (i=0; i+2<n; i++){
         c=a+b;
         cout << c << endl;
         a=b;
